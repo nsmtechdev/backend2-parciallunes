6003e555ef74ff174e34faa1784019254e09664d
nsmtechdev <nsmtechdev@gmail.com>
2023-03-20 15:17:04 -0300

    eureka+gateway2

---
+++ eurekaService/src/test/java/com/dh/eurekaService/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.eurekaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
